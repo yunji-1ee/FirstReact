8cce5f206bf396d1e023465f53a9971169f933ea
yunji-1ee <yunji011219@gmail.com>
2024-11-09 23:54:31 +0900

    add

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          이윤지의 로컬호스트입니당 ㅎ
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/yunji-1ee"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
